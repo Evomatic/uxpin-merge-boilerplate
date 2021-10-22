c00cc61e55782635717e186123343ac28f11a8d1
Evan Trujillo <evantrujillo@Evans-MacBook-Pro.local>
2021-10-22 09:38:55 +0900

    test_4

--- uxpin.config.js
+++ uxpin.config.js
@@ -7,7 +7,7 @@ module.exports = {
         include: [
           'src/components/Camera/Camera.js',
           'src/components/CreditCard/CreditCard.jsx',
-          'src/components/Greeting/Greeting.js',
+          // 'src/components/Greeting/Greeting.js',
           'src/components/Icon/Icon.js',
           'src/components/MediaPlayer/MediaPlayer.js',
           'src/components/Table/Table.js',
@@ -18,7 +18,7 @@ module.exports = {
       {
         name: 'Form',
         include: [
-          // 'src/components/Button/Button.js',
+          'src/components/Button/Button.js',
           'src/components/Select/Select.js',
           'src/components/Select/components/SelectItem/SelectItem.js',
           'src/components/TextField/TextField.jsx',
