91dff9920d0a8e8836d0fa6dec53e371e7cdf9bf
Evan Trujillo <evantrujillo@Evans-MacBook-Pro.local>
2021-10-22 11:09:52 +0900

    test11

--- uxpin.config.js
+++ uxpin.config.js
@@ -7,7 +7,7 @@ module.exports = {
         include: [
           'src/components/Camera/Camera.js',
           'src/components/CreditCard/CreditCard.jsx',
-          // 'src/components/Greeting/Greeting.js',
+          'src/components/Greeting/Greeting.js',
           // 'src/components/Icon/Icon.js',
           // 'src/components/MediaPlayer/MediaPlayer.js',
           // 'src/components/Table/Table.js',
