2c172860f8a152c89576512c9f238e6c5eb3d5df
Evan Trujillo <evantrujillo@Evans-MacBook-Pro.local>
2021-10-25 11:04:29 +0900

    updated merge/cli

--- uxpin.config.js
+++ uxpin.config.js
@@ -6,8 +6,8 @@ module.exports = {
         name: 'General',
         include: [
           'src/components/Camera/Camera.js',
-          'src/components/CreditCard/CreditCard.jsx',
-          'src/components/Greeting/Greeting.js',
+          // 'src/components/CreditCard/CreditCard.jsx',
+          // 'src/components/Greeting/Greeting.js',
           'src/components/Icon/Icon.js',
           'src/components/MediaPlayer/MediaPlayer.js',
           'src/components/Table/Table.js',
